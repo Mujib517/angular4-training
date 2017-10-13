3dc394daba20f775a4494eda258c3893a4ecd7cb
Mujib517 <mujib517@gmail.com>
2017-10-13 09:10:12 +0530

    Change detection

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../../shared/task.service';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,38 @@ import { Component, OnInit } from '@angular/core';
     <h1 [dlShow]="true">Show directive demo</h1>
 
     <h1 *dlIf="false">If Directive Demo</h1>
+
+    
+    <h1>{{count}}</h1>
+
+    <button class="btn btn-success" (click)="inc()">Increment</button>
+    <button class="btn btn-success" (click)="call()">Make Call</button>
+
+
   `,
   styles: []
 })
-export class HomeComponent { }
+export class HomeComponent {
+  count = 0;
+  interval;
+
+  //change detection
+  constructor(private taskSvc: TaskService) {
+    this.interval = setInterval(() => this.count++, 1000);
+  }
+
+  inc() {
+    this.count++;
+  }
+
+  call() {
+    this.taskSvc.get()
+      .subscribe(
+      () => this.count = 10
+      )
+  }
+
+  ngOnDesstroy() {
+    this.interval.clearInterval();
+  }
+}
